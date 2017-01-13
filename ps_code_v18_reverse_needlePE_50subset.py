f59d77598c044831ac41f466b174b5392f77dc0e
Peter Su <su.peter92@gmail.com>
2017-01-13 10:38:11 -0600

    Now runs 1000 sequences

--- ps_code_v18_reverse_needlePE_50subset.py
+++ ps_code_v18_reverse_needlePE_50subset.py
@@ -136,11 +136,11 @@ def filter_sample(f_name,pe_name,template,f_filt_seqs,r_filt_seqs):
         #overall kind of clunky. 
 
         f_seqs3 = f_seqs2[0]
-        f_seqs4 = f_seqs3[0:49]
+        f_seqs4 = f_seqs3[0:1000]
         # Repeat for paired-end reads
 
         pe_seqs3 = pe_seqs2[2]
-        pe_seqs4 = pe_seqs3[0:49]
+        pe_seqs4 = pe_seqs3[0:1000]
         print(str(len(f_seqs3))+' forward reads have the sequence of interest (MBP forward primer)')
         print(str(len(pe_seqs3))+' paired-end reads have the sequence of interest (transposon scar)')
         # Now that only sequences containing BOTH the CS and the TR have been filtered for,
