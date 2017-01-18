89327c8e5a533c04bc58f6155bc4aa026087253a
Peter Su <su.peter92@gmail.com>
2017-01-18 15:20:20 -0600

    Now runs all sequences

--- ps_code_v18_reverse_needlePE_50subset.py
+++ ps_code_v18_reverse_needlePE_50subset.py
@@ -136,17 +136,17 @@ def filter_sample(f_name,pe_name,template,f_filt_seqs,r_filt_seqs):
         #overall kind of clunky. 
 
         f_seqs3 = f_seqs2[0]
-        f_seqs4 = f_seqs3[0:1000]
+
         # Repeat for paired-end reads
 
         pe_seqs3 = pe_seqs2[2]
-        pe_seqs4 = pe_seqs3[0:1000]
+
         print(str(len(f_seqs3))+' forward reads have the sequence of interest (MBP forward primer)')
         print(str(len(pe_seqs3))+' paired-end reads have the sequence of interest (transposon scar)')
         # Now that only sequences containing BOTH the CS and the TR have been filtered for,
         # the paired-end matching can occur
         
-        s1 = filter_pe_mismatch(f_seqs4,pe_seqs4,gen_copied_seq_function(f_res),f_filt_seqs[2]) #right now using the scar
+        s1 = filter_pe_mismatch(f_seqs3,pe_seqs3,gen_copied_seq_function(f_res),f_filt_seqs[2]) #right now using the scar
         seqs = s1[0]
         with open('matched_seq_PE.fa','w') as sh: #create temporary seq file, hopefully re-written each time
                 #temp_f_seq = copied
