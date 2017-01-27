f9270c6f1dbcb34010dc16dc3dac2a345d75574a
Peter Su <su.peter92@gmail.com>
2017-01-27 10:46:44 -0600

    Changed indices to remove any and all end dashes in PE mismatch

--- ps_code_v18_reverse_needlePE_50subset.py
+++ ps_code_v18_reverse_needlePE_50subset.py
@@ -425,7 +425,7 @@ def filter_pe_mismatch(f_seqs,pe_seqs,copied_func,filt_seq): #Now edited to use 
                     hi_cutoff = 1500
                     cond_bar = re.search('[AGCT]+',str(aln_data[0][1].seq)) #search forwards through reverse complement of PE read, find first base that aligned.
                     cond_bar_f = re.search('[AGCT]+',str(aln_data[0][0].seq)[-1:0:-1]) #search backwards through forward read, find first base that aligned
-                    cond_match_coord_start = cond_bar.span()[0] #coordinates start from the first base of the forward read that aligned with the paired end read
+                    cond_match_coord_start = max([cond_bar.span()[0],len(aln_data[0][0].seq)-cond_bar_f.span()[1]]) #coordinates start from the first base of the forward read that aligned with the paired end read
                     cond_match_coord_end = len(aln_data[0][0].seq)-cond_bar_f.span()[0]
                     cond_search_oligo = str(aln_data[0][0].seq)[cond_match_coord_start:cond_match_coord_end]
                     scores = score_cutoff_by_length(cond_search_oligo,bin_scores)
