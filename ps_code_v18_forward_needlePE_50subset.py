414974c26935ed14953fb1894fcbe0692d242435
Peter Su <Peter Su>
2017-02-04 13:32:01 -0600

    Made sure to open copied sequence for needle scoring.

--- ps_code_v18_forward_needlePE_50subset.py
+++ ps_code_v18_forward_needlePE_50subset.py
@@ -430,7 +430,7 @@ def filter_pe_mismatch(f_seqs,pe_seqs,copied_func,filt_seq): #Now edited to use 
                     needle_cline = NeedleCommandline(asequence='temp_seq_PE.fa', bsequence='temp_temp_PE.fa', gapopen=10,
                                                      gapextend=0.5, outfile='PE_copied.needle') #hopefully only one needle file gets made
                     needle_cline()
-                    aln_data = list(AlignIO.parse(open('PE.needle'),"emboss"))
+                    aln_data = list(AlignIO.parse(open('PE_copied.needle'),"emboss"))
                     bin_scores = [[46,251],[213,501],[458,751],[703,1001],[952,1251],[1128,1500],[1400,1750],[1650,2000],[1800,2250],[2150,2500]] #same bin cutoff scores as alignment
                     #initialize cutoff scores
                     lo_cutoff = 0
