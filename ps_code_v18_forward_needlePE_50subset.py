4c45cd7c795a09edee396716f570895fab7ae8f5
Peter Su <Peter Su>
2017-02-17 09:32:21 -0600

    Replaced insertion chunks with the one from postneedlev10 forward

--- ps_code_v18_forward_needlePE_50subset.py
+++ ps_code_v18_forward_needlePE_50subset.py
@@ -537,7 +537,6 @@ def insertion_chunks(final_seqs):
     Input:
         final_seqs - list of SeqRecord objects containing the filtered sequences from the 
         EMBOSS needle alignment algorithm. 
-        template - str, DNA sequence of the template
         
     Outputs:
         chunk_dict - dict of chunks of continuous DNA segments per read.
@@ -550,76 +549,36 @@ def insertion_chunks(final_seqs):
     chunk_dict = {}
     insertions = []
     insertions_corrected = []
-    chunk_size = 0
-    max_chunks = 0
     reads_at_end = 0
     large_chunk_reads = 0
-    end_dashes = 0
     perfect_matches = 0
-    max_chunks_exceeded = 0
     other_scenario = 0
     discarded_reads = 0
 
-    for i in range(len(final_seqs)):
-           # end_pos = 0 #forward search starts at the beginning
-           #insert_site = 0
+    for i in final_seqs:
            num_chunks = 0
            seq_chunks = []
            insert_site = 0
-           total_len = 0
-           #print('Current sequence: ' +str(i+1)) #keep this only for test sequences
-           if str(final_seqs[i].seq)[-1] == '-':
-              discarded_reads += 1
-              end_dashes +=1
-              continue
-           while total_len < len(final_seqs[i].seq):
-              bar=re.search('[AGCT]+',str(final_seqs[i].seq)) #forward search: from start to finish
-              if str(type(bar)) == "<type 'NoneType'>":
+           bar=re.search('[AGCT]+',str(i.seq)) #forward search: from start to finish
+           if str(type(bar)) == "<type 'NoneType'>":
                    #If this happens, we'll know the end was reached without finding a suitable insertion
-                    reads_at_end += 1
-                    break
+              reads_at_end += 1
+              continue
 
-              # if insert_site >= 300: #this prevents a nonphysical insertion from happening
-              #       insert_site = insert_site-4
-              #       insertions.append(insert_site)
-              #       break
-              elif abs((bar.span()[1]-bar.span()[0])) == (len(final_seqs[i].seq.lstrip('-').strip('-'))): #perfect match occurs
-                     insert_site = bar.span()[0] #forward search stops at the first base of the DNA chunk
-                     insertions.append(insert_site)
-                     chunk_dict.update({insert_site:seq_chunks})
-                     seq_chunks.append(bar.span()[1]-bar.span()[0])
-                     perfect_matches +=1
-                     break
-              # elif abs((bar.span()[1]-bar.span()[0])) <= chunk_size: #if a chunk is small enough, set index correspondingly but keep searching through the alignment
-              #        num_chunks += 1
-              #        end_pos += bar.span()[1]
-              #        insert_site += bar.span()[1]
-              #        span_length = abs(bar.span()[1]-bar.span()[0])
-              #        seq_chunks.append(span_length)
-              #        total_len += bar.span()[1]
-              #        continue
-              # elif (abs((bar.span()[1]-bar.span()[0])) > chunk_size) and (abs((bar.span()[1]-bar.span()[0])) != len(final_seqs[i].seq.lstrip('-'))-total_len): #if chunk too large, get rid of the alignment
-              #        discarded_reads += 1
-              #        large_chunk_reads +=1
-              #        break
-              # elif len(final_seqs[i].seq.strip('-')) != len(final_seqs[i].seq.lstrip('-')): #gets rid of alignments with gaps at the 3' end
-              #        discarded_reads +=1
-              #        break
-              elif num_chunks > max_chunks: #too many chunks leads to an alignment being thrown out. 
-                     discarded_reads += 1
-                     max_chunks_exceeded +=1
-                     break
-         
-              else: #This should not happen now, but if it does, will document it
-                    other_scenario +=1
-                    discarded_reads += 1
-                    break
-    insertions_corrected = [s+4 for s in insertions] #correcting for the fact that this is the back end of the duplicated 5 bp. only 4 is added since we define insert site as inserting after that position
+           elif abs((bar.span()[1]-bar.span()[0])) == (len(i.seq.strip('-'))): #perfect match occurs
+              insert_site = bar.span()[0] #forward search stops at the first base of the DNA chunk
+              insertions.append(insert_site)
+              chunk_dict.update({insert_site:seq_chunks})
+              seq_chunks.append(bar.span()[1]-bar.span()[0])
+              perfect_matches +=1
+              continue
+
+           else: #This should not happen now, but if it does, will document it
+              other_scenario +=1
+              continue
+    insertions_corrected = [s+4 for s in insertions] #must add 4 to all forward searching regex searches to account for the duplication 
+    # (and since the insertion is defined as inserting after the indexed base)
     print (str(reads_at_end)+ ' reads reached the end without a suitable insertion')    
-    print (str(discarded_reads)+' reads were discarded :(')
-    print(str(large_chunk_reads)+' reads had too large of a chunk')
-    print(str(max_chunks_exceeded)+' reads had too many chunks')
-    print(str(end_dashes)+' reads had end dashes')
     print(str(perfect_matches)+' reads are perfect matches')
     print(str(other_scenario) +' reads did not satisfy any of the criteria')
 
